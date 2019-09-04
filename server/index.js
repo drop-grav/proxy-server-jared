1c866e70d18abab62ae75b4e3b64daf807d7624d
root <adrianligad@yahoo.com>
2019-09-04 14:40:18 -0700

    complete proxy

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require ('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser');  
+
+app.use(`/listing/:id`, express.static('public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.listen(port, ()=> console.log(`Reservations module listening on port ${port}`))
