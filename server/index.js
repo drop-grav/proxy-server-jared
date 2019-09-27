2e74a6d9af402eedc59cdaaa15ae38b34e9e244f
EC2 Default User <ec2-user@ip-172-31-31-111.us-east-2.compute.internal>
2019-09-27 20:24:42 +0000

    update proxy server for aws

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser');  
 
+app.use(express.static('public'));
 app.use(`/listing/:id`, express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
